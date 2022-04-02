ca092cba70a66f97527ff12aef6a2579a34cee84
KleeSup <45318360+KleeSup@users.noreply.github.com>
2022-04-03 00:49:09 +0200

    Updated TileChangeListener

--- src/main/java/com/github/kleesup/CloverGdxUtils/tiled/TileChangeListener.java
+++ src/main/java/com/github/kleesup/CloverGdxUtils/tiled/TileChangeListener.java
@@ -10,16 +10,18 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
  *
  * A listener class which is used in {@link TiledMapTileChangeLayer} to listen to cell/tile changes.
  */
-public interface TileChangeListener {
+public abstract class TileChangeListener {
 
-    default void onCellChange(final TilePosition position, final TiledMapTileLayer.Cell cell, final TiledMapTileLayer.Cell oldCell){
+    public abstract void onCellChange(final TiledMapTileLayer layer, int x, int y, final TiledMapTileLayer.Cell cell, final TiledMapTileLayer.Cell oldCell);
+
+    public void onCellChange(final TilePosition position, final TiledMapTileLayer.Cell cell, final TiledMapTileLayer.Cell oldCell){
         onCellChange(position.getLayer(),position.getX(),position.getY(),cell,oldCell);
     }
-    void onCellChange(final TiledMapTileLayer layer, int x, int y, final TiledMapTileLayer.Cell cell, final TiledMapTileLayer.Cell oldCell);
 
-    default void onTileChange(final TilePosition position, final TiledMapTileLayer.Cell cell, final TiledMapTile oldTile){
+    public abstract void onTileChange(final TiledMapTileLayer layer, int x, int y, final TiledMapTileLayer.Cell cell, final TiledMapTile oldTile);
+
+    public void onTileChange(final TilePosition position, final TiledMapTileLayer.Cell cell, final TiledMapTile oldTile){
         onTileChange(position.getLayer(), position.getX(), position.getY(), cell, oldTile);
     }
-    void onTileChange(final TiledMapTileLayer layer, int x, int y, final TiledMapTileLayer.Cell cell, final TiledMapTile oldTile);
 
 }
